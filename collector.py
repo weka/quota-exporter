fd042f823538ace1c68ebddc58e2d10621a0bb1d
Vince Fleming <vince@weka.io>
2021-10-01 08:18:03 -0400

    Initial commit

---
+++ collector.py
@@ -0,0 +1,190 @@
+import sys
+import time
+import traceback
+from logging import getLogger
+from threading import Lock
+
+# initialize logger - configured in main routine
+import wekalib
+from prometheus_client.core import GaugeMetricFamily
+
+log = getLogger(__name__)
+
+
+class Collector(object):
+    def __init__(self, config, cluster_obj):  # wekaCollector
+
+        # dynamic module globals
+        buckets = []  # same for everyone
+        self._access_lock = Lock()
+        self.gather_timestamp = None
+        self.collect_time = None
+        self.clusterdata = {}
+        self.threaderror = False
+        self.api_stats = {}
+        self.backends_only = config['exporter']['backends_only']
+
+        self.cluster = cluster_obj
+
+    def collect(self):
+
+        global quota_objs
+
+        # lock_starttime = time.time()
+        with self._access_lock:  # be thread-safe, and don't conflict with events collection
+            # log.info(f"Waited {round(time.time() - lock_starttime, 2)}s to obtain lock")
+
+            self.api_stats['num_calls'] = 0
+
+            log.debug("Entering collect() routine")
+            second_pass = False
+            should_gather = False
+
+            # get the time once here
+            start_time = time.time()
+
+            # first time being called?   force gathering info?
+            if self.collect_time is None:
+                log.debug("never gathered before")
+                should_gather = True
+            elif start_time - self.collect_time > 5:  # prometheus always calls twice; only gather if it's been a while since last call
+                should_gather = True
+            else:
+                second_pass = True
+
+            if should_gather:
+                log.info("gathering")
+                try:
+                    quota_objs = self.gather()
+                except wekalib.exceptions.NameNotResolvable as exc:
+                    log.critical(f"Unable to resolve names; terminating")
+                    sys.exit(1)
+                except Exception as exc:
+                    log.critical(f"Error gathering data: {exc}, {traceback.format_exc()}")
+                    # log.critical(traceback.format_exc())
+                    return  # raise?
+
+            # yield for each metric
+            log.debug("Yielding metrics")
+            yield quota_objs
+            log.debug("Yielding complete")
+
+            # report time if we gathered, otherwise, it's meaningless
+            if should_gather:
+                self.last_elapsed = time.time() - start_time
+            else:
+                elapsed = self.last_elapsed
+
+            log.debug("Yielding process metrics")
+            yield GaugeMetricFamily('weka_collect_seconds', 'Total Time spent in Prometheus collect',
+                                    value=self.last_elapsed)
+            yield GaugeMetricFamily('weka_collect_apicalls', 'Total number of api calls',
+                                    value=self.api_stats['num_calls'])
+            log.debug("Yielding process metrics complete")
+
+            if not second_pass:
+                log.info(
+                    f"stats returned. total time = {round(self.last_elapsed, 2)}s {self.api_stats['num_calls']} api calls made. {time.asctime()}")
+            self.collect_time = time.time()
+
+    def gather(self):
+
+        # reset the cluster config to be sure we can talk to all the hosts
+        try:
+            self.cluster.refresh()
+        except wekalib.exceptions.NameNotResolvable as exc:
+            log.critical(f"Names are not resolvable - are they in /etc/hosts or DNS? {exc}")
+            raise
+        except Exception as exc:
+            log.error(f"Cluster refresh failed on cluster '{self.cluster}' - check connectivity ({exc})")
+            # log.error(traceback.format_exc())
+            return
+
+        # value is the total bytes used
+        quota_gauge = GaugeMetricFamily("weka_quota", "Weka Directory Quota",
+                                        labels=["cluster",
+                                                "filesystem",
+                                                "directory",
+                                                "owner",
+                                                "soft_quota",
+                                                "hard_quota",
+                                                "bytes_used"])
+
+        # we have to ask for quotas for each FS individually, so get a list of filesystems
+        filesystems = self.get_filesystems()
+
+        # for each FS, ask for quotas... this may require several calls
+        for fs in filesystems:
+            quotas = self.get_quotas(fs)
+            for quota, details in quotas.items():
+                if details['totalBytes'] > details['softLimitBytes']:
+                    dirname = self.resolve_dirname(details)
+                    quota_gauge.add_metric([str(self.cluster), fs, dirname, details['owner'],
+                                            str(details['softLimitBytes']),
+                                            str(details['hardLimitBytes'])],
+                                           details['totalBytes'])
+
+        return quota_gauge
+
+    # returns a list of filesystem names
+    def get_filesystems(self):
+        try:
+            filesystems_cap = self.cluster.call_api(method="filesystems_get_capacity", parms={})
+        except Exception as exc:
+            log.error(f"Error querying cluster for FS names: {exc}")
+            return None
+
+        fsnames = list()
+
+        # filesystems_cap is a list of dict's
+        for fs in filesystems_cap:
+            fsnames.append(fs['name'])
+
+        return fsnames
+
+    def get_quotas(self, fs_name):
+        #try:
+        #    result = self.cluster.call_api(method='directory_quota_list', parms={"fs_name": fs_name, "start_cookie": 0})
+        #except Exception as exc:
+        #    log.error(f"Error fetching quotas for fs {fs_name}: {exc} {traceback.format_exc()}")
+        #    return None
+
+        #nextCookie = result['nextCookie']
+        #all_quotas = result['quotas']
+        #quotas = all_quotas
+        first_time = True
+        nextCookie = 0
+        all_quotas = dict()
+        quotas = dict()
+        start_time = time.time()
+
+        while len(quotas) > 0 or first_time:
+            first_time = False
+            this_call_start = time.time()
+            try:
+                result = self.cluster.call_api(method='directory_quota_list',
+                                               parms={"fs_name": fs_name, "start_cookie": nextCookie})
+            except Exception as exc:
+                log.error(f"Error fetching more quotas for fs {fs_name}: {exc}")
+                return None
+            log.info(f"ET for api call: {time.time() - this_call_start}")
+            nextCookie = result['nextCookie']
+            quotas = result['quotas']
+            all_quotas.update(quotas)
+            log.info(f"number of quotas returned is {len(quotas)}")
+            #all_quotas += quotas
+
+        log.info(f"ET for filesystem '{fs_name}': {time.time() - start_time}; total quotas is {len(all_quotas)}")
+        return all_quotas
+
+    def resolve_dirname(self, quota):
+        start_time = time.time()
+        try:
+            result = self.cluster.call_api(method='filesystem_resolve_inode',
+                                           parms={'inodeContext': quota['inodeId'], 'snapViewId': quota['snapViewId']})
+        except Exception as exc:
+            log.error(f"Error resolving directory name: {exc}")
+            return None
+
+        log.info(f"ET to resolve name: {time.time() - start_time}")
+        return result['path']
---
+++ main.py
@@ -0,0 +1,208 @@
+import argparse
+import logging
+import logging.handlers
+import os
+import platform
+import socket
+import sys
+import time
+import traceback
+
+import prometheus_client
+
+import wekalib
+import yaml
+from wekalib import signal_handling, WekaCluster
+
+from collector import Collector
+
+VERSION = "0.1.0"
+
+# set the root log
+log = logging.getLogger()
+
+def _load_config(inputfile):
+    try:
+        f = open(inputfile)
+    except Exception as exc:
+        raise
+    with f:
+        try:
+            return yaml.load(f, Loader=yaml.FullLoader)
+        except AttributeError:
+            return yaml.load(f)
+        except Exception as exc:
+            log.error(f"Error reading config file: {exc}")
+            raise
+
+def prom_client(config):
+
+    error = False
+    for host in config['cluster']['hosts']:
+        try:
+            socket.gethostbyname(host)
+        except socket.gaierror:
+            log.critical(f"Hostname {host} not resolvable - is it in /etc/hosts or DNS?")
+            error = True
+        except Exception as exc:
+            log.critical(exc)
+            error = True
+
+    if error:
+        log.critical("Errors resolving hostnames given.  Please ensure they are in /etc/hosts or DNS and are resolvable")
+        sys.exit(1)
+    elif 'cluster' not in config:
+        log.error(f"'cluster:' stanza missing from .yml file - version mismatch between .yml and exporter version?")
+        sys.exit(1)
+    elif 'exporter' not in config:
+        log.error(f"'exporter:' stanza missing from .yml file - version mismatch between .yml and exporter version?")
+        sys.exit(1)
+
+    if 'force_https' not in config['cluster']:  # allow defaults for these
+        config['cluster']['force_https'] = False
+
+    if 'verify_cert' not in config['cluster']:
+        config['cluster']['verify_cert'] = True
+
+    if 'timeout' not in config['exporter']:
+        config['exporter']['timeout'] = 10
+
+    if 'backends_only' not in config['exporter']:
+        config['exporter']['backends_only'] = True
+
+    log.info(f"Timeout set to {config['exporter']['timeout']} secs")
+
+    try:
+        cluster_obj = WekaCluster(config['cluster']['hosts'], config['cluster']['auth_token_file'],
+                                  force_https=config['cluster']['force_https'],
+                                  verify_cert=config['cluster']['verify_cert'],
+                                  backends_only=config['exporter']['backends_only'],
+                                  timeout=config['exporter']['timeout'])
+    except wekalib.exceptions.HTTPError as exc:
+        if exc.code == 403:
+            log.critical(f"Cluster returned permission error - is the userid level ReadOnly or above?")
+            return
+        log.critical(f"Cluster returned HTTP error {exc}; aborting")
+        return
+    except wekalib.exceptions.SSLError as exc:
+        log.critical(f"SSL Error: Only weka v3.10 and above support https, and force_https is set in config file.")
+        log.critical(f"SSL Error: Is this cluster < v3.10? Please verify configuration")
+        log.critical(f"Error is {exc}")
+        return
+    except Exception as exc:
+        log.critical(f"Unable to create Weka Cluster: {exc}")
+        log.critical(traceback.format_exc())
+        return
+
+    # create the WekaCollector object
+    collector = Collector(config, cluster_obj)
+
+    #
+    # Start up the server to expose the metrics.
+    #
+    log.info(f"starting http server on port {config['exporter']['listen_port']}")
+    try:
+        prometheus_client.start_http_server(int(config['exporter']['listen_port']))
+    except Exception as exc:
+        log.critical(f"Unable to start http server on port {config['exporter']['listen_port']}: {exc}")
+        return 1
+
+    # register our custom collector
+    prometheus_client.REGISTRY.register(collector)
+
+    while True:
+        time.sleep(30)  # let prometheus_client call-back do it's thing
+
+
+def configure_logging(logger, verbosity):
+    loglevel = logging.INFO     # default logging level
+    libloglevel = logging.ERROR
+
+    # default message formats
+    console_format = "%(message)s"
+    syslog_format =  "%(levelname)s:%(message)s"
+
+    syslog_format =  "%(process)s:%(filename)s:%(lineno)s:%(funcName)s():%(levelname)s:%(message)s"
+
+    if verbosity == 1:
+        loglevel = logging.INFO
+        console_format = "%(levelname)s:%(message)s"
+        syslog_format =  "%(process)s:%(filename)s:%(lineno)s:%(funcName)s():%(levelname)s:%(message)s"
+        libloglevel = logging.INFO
+    elif verbosity == 2:
+        loglevel = logging.DEBUG
+        console_format = "%(filename)s:%(lineno)s:%(funcName)s():%(levelname)s:%(message)s"
+        syslog_format =  "%(process)s:%(filename)s:%(lineno)s:%(funcName)s():%(levelname)s:%(message)s"
+    elif verbosity > 2:
+        loglevel = logging.DEBUG
+        console_format = "%(filename)s:%(lineno)s:%(funcName)s():%(levelname)s:%(message)s"
+        syslog_format =  "%(process)s:%(filename)s:%(lineno)s:%(funcName)s():%(levelname)s:%(message)s"
+        libloglevel = logging.DEBUG
+
+
+    # create handler to log to console
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter(console_format))
+    logger.addHandler(console_handler)
+
+    # create handler to log to syslog
+    logger.info(f"setting syslog on {platform.platform()}")
+    if platform.platform()[:5] == "macOS":
+        syslogaddr = "/var/run/syslog"
+    else:
+        syslogaddr = "/dev/log"
+    syslog_handler = logging.handlers.SysLogHandler(syslogaddr)
+    syslog_handler.setFormatter(logging.Formatter(syslog_format))
+
+    # add syslog handler to root logger
+    if syslog_handler is not None:
+        logger.addHandler(syslog_handler)
+
+    # set default loglevel
+    logger.setLevel(loglevel)
+
+    logging.getLogger("wekalib").setLevel(logging.ERROR)
+    logging.getLogger("wekalib.wekaapi").setLevel(libloglevel) # should leave at INFO as default
+    logging.getLogger("wekalib.wekacluster").setLevel(libloglevel)
+    logging.getLogger("wekalib.sthreads").setLevel(logging.ERROR) # should leave at ERROR as default
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
+
+    # local modules
+    logging.getLogger("collector").setLevel(loglevel)
+    logging.getLogger("lokilogs").setLevel(loglevel)
+    logging.getLogger("async_api").setLevel(loglevel)
+
+def main():
+    signal_handling()
+
+    parser = argparse.ArgumentParser(description="Prometheus Client for Weka clusters")
+    parser.add_argument("-c", "--configfile", dest='configfile', default="./quota-export.yml",
+                        help="override ./quota-export.yml as config file")
+    parser.add_argument("-v", "--verbosity", action="count", default=0, help="increase output verbosity")
+    parser.add_argument("--version", dest="version", default=False, action="store_true", help="Display version number")
+    args = parser.parse_args()
+
+    if args.version:
+        print(f"{sys.argv[0]} version {VERSION}")
+        sys.exit(0)
+
+    configure_logging(log, args.verbosity)
+
+    if not os.path.exists(args.configfile):
+        log.critical(f"Required configfile '{args.configfile}' does not exist")
+        sys.exit(1)
+
+    log.debug("loading config file")
+    try:
+        config = _load_config(args.configfile)
+    except Exception as exc:
+        log.critical(f"Error loading config file '{args.configfile}': {exc}")
+        return
+    log.debug("config file loaded")
+
+    prom_client(config)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
